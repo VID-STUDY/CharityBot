b1278ce9229cbce6a792a3c46487bcc2577b8941
dmitriyl99 <dmitriyl1899@gmail.com>
2020-07-26 19:53:29 +0500

    Initial models

--- charity/models.py
+++ charity/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class TelegramUser(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    language = models.CharField(max_length=2)
+
+
+class HelpRequest(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    description = models.CharField(max_length=1024)
+    help_type = models.CharField(max_length=20)
+    user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
