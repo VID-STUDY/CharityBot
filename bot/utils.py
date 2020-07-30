8d2be1922d74ac7fff4c529023ce76e0f180aa8d
dmitriyl99 <dmitriyl1899@gmail.com>
2020-07-30 14:18:42 +0500

    Fix issue with conditions

--- bot/utils.py
+++ bot/utils.py
@@ -4,8 +4,10 @@ from telegram.ext import BaseFilter
 
 class Navigation:
     @staticmethod
-    def to_main_menu(update, context):
+    def to_main_menu(update, context, message=None):
         main_menu_message = strings.get_string('main_menu', context.user_data['user'].language)
+        if message:
+            main_menu_message = message
         main_menu_keyboard = keyboards.get_keyboard('main_menu', context.user_data['user'].language)
         update.message.reply_text(text=main_menu_message, reply_markup=main_menu_keyboard)
 
@@ -14,5 +16,10 @@ class CharityFilters:
 
     class NeedHelpFilter(BaseFilter):
         def filter(self, message):
-            return message.text and (strings.get_string('menu.need_help', 'ru') in message.text) or 
-                                    (strings.get_string('menu.need_help', 'uz') in message.text)
+            return message.text and ((strings.get_string('menu.need_help', 'ru') in message.text) or 
+                                    (strings.get_string('menu.need_help', 'uz') in message.text))
+    
+    class CancelFilter(BaseFilter):
+        def filter(self, message):
+            return message.text and ((strings.get_string('cancel', 'ru') in message.text) or
+                                    (strings.get_string('cancel', 'uz') in message.text))
