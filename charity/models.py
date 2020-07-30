5c3e1dd3085991d1a1b2315ffcd8a35cb9d5f922
dmitriyl99 <dmitriyl1899@gmail.com>
2020-07-30 15:44:53 +0500

    Get latest HelpRequest by created_at field

--- charity/models.py
+++ charity/models.py
@@ -16,3 +16,6 @@ class HelpRequest(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
+    class Meta:
+        get_latest_by = 'created_at'
+
