3a75d872eaa157d323505f40085a44e44d59336b
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-05 02:21:58 +0500

    Fix issue with getenv

--- charity/views/bot.py
+++ charity/views/bot.py
@@ -2,6 +2,8 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 
+from os import getenv
+
 
 class StartBotView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
