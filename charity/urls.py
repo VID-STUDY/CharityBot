5601c6bbb7894c28fd91e59741b73ff5b271daa9
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-04 14:02:33 +0500

    Users in admin

--- charity/urls.py
+++ charity/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from .views.dashboard import DashboardView
+from .views.users import TelegramUserList, TelegramUserDelete, TelegramUserDetail
 
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='admin-home')
+    path('', DashboardView.as_view(), name='admin-home'),
+    path('users/', TelegramUserList.as_view(), name='admin-users-index'),
+    path('users/<int:pk>/', TelegramUserDetail.as_view(), name='admin-users-detail'),
+    path('users/<int:pk>/delete/', TelegramUserDelete.as_view(), name='admin-users-delete')
 ]
 
