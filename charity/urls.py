c390e59c22056886a1cec61159051fafd4d37092
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-04 17:15:21 +0500

    Help Request in Dashboard

--- charity/urls.py
+++ charity/urls.py
@@ -2,12 +2,18 @@ from django.urls import path
 
 from .views.dashboard import DashboardView
 from .views.users import TelegramUserList, TelegramUserDelete, TelegramUserDetail
+from .views.helprequests import HelpRequestsList, HelpRequestDetail, HelpRequestDelete, HelpRequestResolveConflict
 
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='admin-home'),
     path('users/', TelegramUserList.as_view(), name='admin-users-index'),
     path('users/<int:pk>/', TelegramUserDetail.as_view(), name='admin-users-detail'),
-    path('users/<int:pk>/delete/', TelegramUserDelete.as_view(), name='admin-users-delete')
+    path('users/<int:pk>/delete/', TelegramUserDelete.as_view(), name='admin-users-delete'),
+
+    path('help/', HelpRequestsList.as_view(), name='admin-help-index'),
+    path('help/<int:pk>/', HelpRequestDetail.as_view(), name='admin-help-detail'),
+    path('help/<int:pk>/delete', HelpRequestDelete.as_view(), name='admin-help-delete'),
+    path("help/<int:pk>/resolve/", HelpRequestResolveConflict.as_view(), name="admin-help-resolve")
 ]
 
