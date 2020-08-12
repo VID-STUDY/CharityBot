6cdeeef727d081c09f96d151a8eba46d4a02a09d
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-12 14:24:01 +0500

    Clear request reaction after an hour

--- bot/help.py
+++ bot/help.py
@@ -55,6 +55,8 @@ def owner_reaction(update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=reaction.helper_id, text=message)
     elif not reaction.helper_reaction:
         # If owner said "No" but helper haven't responded yet
+        clear_request_timer = Timer(3600, clear_request_reactions, (context, help_request, user))
+        clear_request_timer.start()
         message = strings.get_string('reaction.owner.no_helper_reaction', user.language)
         query.edit_message_text(text=message)
     elif reaction.helper_reaction == HelpRequestReaction.Reactions.YES:
@@ -81,10 +83,13 @@ def helper_reaction(update: Update, context: CallbackContext):
     query = update.callback_query
     reaction_id = query.data.split(':')[1]
     reaction_text = query.data.split(':')[3]
+    user = TelegramUser.objects.get(pk=query.from_user.id)
     try:
         reaction = HelpRequestReaction.objects.get(pk=reaction_id)
     except HelpRequestReaction.DoesNotExist:
-        query.answer()
+        late_message = strings.get_string('reaction.helper.too_late', user.language)
+        query.answer(text=late_message, show_alert=True)
+        context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
         return
     user = TelegramUser.objects.get(pk=query.from_user.id)
     reaction.helper_reaction = reaction_text
@@ -127,6 +132,17 @@ def send_reaction_to_helper(context: CallbackContext, reaction: HelpRequestReact
     helper_reaction_message = strings.reaction_helper_message(reaction.help_request, owner, helper.language)
     helper_reaction_keyboard = keyboards.from_reaction_helper_keyboard(reaction, helper.language)
     context.bot.send_message(chat_id=helper.id, text=helper_reaction_message, reply_markup=helper_reaction_keyboard, parse_mode=ParseMode.HTML)
+
+
+def clear_request_reactions(context: CallbackContext, request: HelpRequest, owner: TelegramUser):
+    if request.is_completted():
+        return
+    request.reaction.delete()
+    request.hidden = False
+    request.has_conflict = False
+    request.save()
+    no_helper_reaction_message = strings.get_string('reaction.owner.no_helper_reaction_yet', owner.language)
+    context.bot.send_message(chat_id=owner.id, text=no_helper_reaction_message)
 
 
 help_handler = CallbackQueryHandler(offer_help, pattern=r'^help:.*')
